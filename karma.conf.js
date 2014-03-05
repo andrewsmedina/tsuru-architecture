52529ba696932d3436328f6d9ebf4a959147b2ef
Andrews Medina <andrewsmedina@gmail.com>
2014-03-05 12:29:00 -0300

    ignoring some files on karma.conf.

--- karma.conf.js
+++ karma.conf.js
@@ -3,11 +3,20 @@ module.exports = function (config) {
 		basePath: '../',
 		frameworks: ['jasmine'],
 		files: [
+		  'app/bower_components/jquery/jquery.js',
 			'app/bower_components/angular/angular.js',
 		  'app/bower_components/**/*.js',
 			'app/scripts/*.js',
 			'app/scripts/controllers/*.js',
 			'test/spec/controllers/*.js'
+		],
+		exclude: [
+			'app/bower_components/jquery/build/*.js',
+			'app/bower_components/jquery/speed/*.js',
+			'app/bower_components/jquery/test/**/*.js',
+			'app/bower_components/sass-bootstrap/js/tests/**/*.js',
+			'app/bower_components/jquery/src/**/*.js',
+			'app/bower_components/es5-shim/**/*.js'
 		],
 		autoWatch: true,
 		browsers: ['Chrome'],
